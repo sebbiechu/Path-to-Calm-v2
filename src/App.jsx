88fe16fc73c9fb4b62620be1c8305c7b554088ab
sebbiechu <139472455+sebbiechu@users.noreply.github.com>
2025-09-16 08:32:29 +0000

    Adding share your feedback button

--- src/App.jsx
+++ src/App.jsx
@@ -241,7 +241,7 @@ export default function App() {
     <path d="M12 20h9" />
     <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z" />
   </svg>
-  <span className="ml-2">&nbsp;Share Your Feedback</span>
+  <span className="ml-2">&nbsp;Share Your Feedback </span>
 </a>
 
 </div>
